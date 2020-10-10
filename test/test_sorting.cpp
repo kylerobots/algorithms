099ebab0c5ebbd32f018f3aace9c98d62efb1566
Kyle Hart <23261132+kylerobots@users.noreply.github.com>
2020-10-10 13:05:17 -0400

    Add insertion sort and associated unit tests.

---
+++ test/test_sorting.cpp
@@ -0,0 +1,59 @@
+#include "gtest/gtest.h"
+#include "sorting.h"
+
+#include <algorithm>
+#include <limits>
+
+TEST(TestSorting, EmptyCase){
+	std::vector<double> input;
+	auto output = Sorting::insertionSort(input);
+	EXPECT_EQ(input, output);
+}
+
+TEST(TestSorting, SingleElement){
+	std::vector<double> input;
+	input.push_back(0.0);
+	auto output = Sorting::insertionSort(input);
+	EXPECT_EQ(input, output);
+}
+
+TEST(TestSorting, Simple){
+	std::vector<double> input {1, 4.5, 3.2, 2.1};
+	auto target_output = input;
+	std::sort(target_output.begin(), target_output.end());
+	auto output = Sorting::insertionSort(input);
+	EXPECT_EQ(output, target_output);
+}
+
+TEST(TestSorting, AlreadySorted){
+	std::vector<double> input;
+	for (size_t i = -10; i < 25; i++)
+	{
+		input.push_back(i);
+	}
+	auto output = Sorting::insertionSort(input);
+	EXPECT_EQ(input, output);
+}
+
+TEST(TestSorting, ReverseSorted){
+	std::vector<double> input;
+	for (size_t i = 25; i > -25; i--)
+	{
+		input.push_back(i);
+	}
+	auto target_output = input;
+	std::sort(target_output.begin(), target_output.end());
+	auto output = Sorting::insertionSort(input);
+	EXPECT_EQ(output, target_output);
+}
+
+TEST(TestSorting, BigNumberSort){
+	std::vector<double> input;
+	input.push_back(0.0);
+	input.push_back(std::numeric_limits<double>::max());
+	input.push_back(std::numeric_limits<double>::min());
+	auto target_output = input;
+	std::sort(target_output.begin(), target_output.end());
+	auto output = Sorting::insertionSort(input);
+	EXPECT_EQ(output, target_output);
+}
