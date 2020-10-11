05c20f958ad7c2b03024d030e63749ac9637541f
Kyle Hart <23261132+kylerobots@users.noreply.github.com>
2020-10-11 17:34:09 -0400

    Change tests to a fixture.

--- test/test_sorting.cpp
+++ test/test_sorting.cpp
@@ -4,54 +4,59 @@
 #include <algorithm>
 #include <limits>
 
-TEST(TestSorting, EmptyCase) {
+// A test fixture to perform the actual sorting.
+class SortFixture : public testing::Test {
+	protected:
+	std::tuple<std::vector<double>, std::vector<double> > sort(const std::vector<double> & input) {
+		auto target_output = input;
+		std::sort(target_output.begin(), target_output.end());
+		auto output = Sorting::insertionSort(input);
+		return std::make_tuple(target_output, output);
+	}
+};
+
+TEST_F(SortFixture, EmptyCase) {
 	std::vector<double> input;
-	auto output = Sorting::insertionSort(input);
-	EXPECT_EQ(input, output);
+	auto results = sort(input);
+	EXPECT_EQ(std::get<0>(results), std::get<1>(results));
 }
 
-TEST(TestSorting, SingleElement) {
+TEST_F(SortFixture, SingleElement) {
 	std::vector<double> input;
 	input.push_back(0.0);
-	auto output = Sorting::insertionSort(input);
-	EXPECT_EQ(input, output);
+	auto results = sort(input);
+	EXPECT_EQ(std::get<0>(results), std::get<1>(results));
 }
 
-TEST(TestSorting, Simple) {
+TEST_F(SortFixture, Simple) {
 	std::vector<double> input {1, 4.5, 3.2, 2.1};
-	auto target_output = input;
-	std::sort(target_output.begin(), target_output.end());
-	auto output = Sorting::insertionSort(input);
-	EXPECT_EQ(output, target_output);
+	auto results = sort(input);
+	EXPECT_EQ(std::get<0>(results), std::get<1>(results));
 }
 
-TEST(TestSorting, AlreadySorted) {
+TEST_F(SortFixture, AlreadySorted) {
 	std::vector<double> input;
 	for (size_t i = -10; i < 25; i++) {
 		input.push_back(i);
 	}
-	auto output = Sorting::insertionSort(input);
-	EXPECT_EQ(input, output);
+	auto results = sort(input);
+	EXPECT_EQ(std::get<0>(results), std::get<1>(results));
 }
 
-TEST(TestSorting, ReverseSorted) {
+TEST_F(SortFixture, ReverseSorted) {
 	std::vector<double> input;
 	for (size_t i = 25; i > -25; i--) {
 		input.push_back(i);
 	}
-	auto target_output = input;
-	std::sort(target_output.begin(), target_output.end());
-	auto output = Sorting::insertionSort(input);
-	EXPECT_EQ(output, target_output);
+	auto results = sort(input);
+	EXPECT_EQ(std::get<0>(results), std::get<1>(results));
 }
 
-TEST(TestSorting, BigNumberSort) {
+TEST_F(SortFixture, NumericalLimits) {
 	std::vector<double> input;
 	input.push_back(0.0);
 	input.push_back(std::numeric_limits<double>::max());
 	input.push_back(std::numeric_limits<double>::min());
-	auto target_output = input;
-	std::sort(target_output.begin(), target_output.end());
-	auto output = Sorting::insertionSort(input);
-	EXPECT_EQ(output, target_output);
+	auto results = sort(input);
+	EXPECT_EQ(std::get<0>(results), std::get<1>(results));
 }
