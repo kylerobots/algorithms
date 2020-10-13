0df8ce28c744c36505fe38d8e9ccdf47ef8f2612
Kyle Hart <23261132+kylerobots@users.noreply.github.com>
2020-10-12 21:29:33 -0400

    Create shell and tests for max subarray.

---
+++ test/test_search.cpp
@@ -0,0 +1,75 @@
+#include "search.h"
+
+#include "gtest/gtest.h"
+
+/**
+ * @brief Test an empty array.
+ * */
+TEST(MaxSubarray, EmptyArray) {
+	std::vector<float> input;
+	EXPECT_THROW(Search::maxSubarray(input), std::length_error);
+}
+
+/**
+ * @brief Test a single element.
+ * */
+TEST(MaxSubarray, SingleElement) {
+	std::vector<float> input;
+	input.push_back(10.0);
+	auto results = Search::maxSubarray(input);
+	EXPECT_EQ(std::get<0>(results), 0);
+	EXPECT_EQ(std::get<1>(results), 0);
+	EXPECT_EQ(std::get<2>(results), input[0]);
+}
+
+/**
+ * @brief Test a monotonic order.
+ * */
+TEST(MaxSubbary, Increasing) {
+	std::vector<float> input;
+	for (double i = -25.0; i <= 25.0; i += 0.3) {
+		input.push_back(i);
+	}
+	auto results = Search::maxSubarray(input);
+	EXPECT_EQ(std::get<0>(results), 0);
+	EXPECT_EQ(std::get<1>(results), input.size() - 1);
+	EXPECT_EQ(std::get<2>(results), 49.8);
+}
+
+/**
+ * @brief Test a decreasing order.
+ * */
+TEST(MaxSubbary, Decreasing) {
+	std::vector<float> input;
+	for (double i = 25.0; i >= -13.0; i -= 0.25) {
+		input.push_back(i);
+	}
+	auto results = Search::maxSubarray(input);
+	EXPECT_EQ(std::get<0>(results), 0);
+	EXPECT_EQ(std::get<1>(results), 0);
+	EXPECT_EQ(std::get<2>(results), input[0]);
+}
+
+/**
+ * @brief Test a single result.
+ * 
+ * This is from the book.
+ * */
+TEST(MaxSubarray, Simple) {
+	std::vector<float> input {100.0, 113.0, 110.0, 85.0, 105.0, 102.0, 86.0, 63.0, 81.0, 101.0, 94.0, 106.0, 101.0, 79.0, 94.0, 90.0, 97.0};
+	auto results = Search::maxSubarray(input);
+	EXPECT_EQ(std::get<0>(results), 7);
+	EXPECT_EQ(std::get<1>(results), 11);
+	EXPECT_EQ(std::get<2>(results), 43.0);
+}
+
+/**
+ * @brief Test when there are two subarrays.
+ * */
+TEST(MaxSubarray, TwoMaxes) {
+	std::vector<float> input {1.0, 2.0, 3.0, -4.0, -5.0, -4.0, -3.0};
+	auto results = Search::maxSubarray(input);
+	EXPECT_EQ(std::get<0>(results), 0);
+	EXPECT_EQ(std::get<1>(results), 2);
+	EXPECT_EQ(std::get<2>(results), 2.0);
+}
